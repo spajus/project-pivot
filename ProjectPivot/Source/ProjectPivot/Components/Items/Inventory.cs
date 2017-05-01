071d2b8248490b96c81f31b74c6501dca5548b58
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2017-05-01 19:15:53 +0300

    Group same inventory items into stacks

--- ProjectPivot/Source/ProjectPivot/Components/Items/Inventory.cs
+++ ProjectPivot/Source/ProjectPivot/Components/Items/Inventory.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using ProjectPivot.Entities;
 using ProjectPivot.Entities.Items;
 
 namespace ProjectPivot.Components.Items {
     public class Inventory : Component {
-        protected List<Item> items;
+        protected Dictionary<string, ItemStack> items;
         public Inventory() {
-            items = new List<Item>();
-
+            items = new Dictionary<string, ItemStack>();
         }
 
         public void Pickup(Item item) {
-            GameObjects.Remove(item); // no longer on the ground
-            items.Add(item);
+            GameObjects.Remove(item);
+            if (items.ContainsKey(item.Id)) {
+                items[item.Id].Add(item);
+            } else {
+                items.Add(item.Id, new ItemStack(item));
+            }
+            Console.WriteLine(this);
+        }
+
+        public override string ToString() {
+            return string.Format("[Inventory items={0}]", 
+               string.Join(", ", items.Select(kvp => kvp.Key + ": " + kvp.Value.ToString())));
         }
     }
 }
---
+++ ProjectPivot/Source/ProjectPivot/Entities/Items/ItemStack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ProjectPivot.Entities.Items {
+    public class ItemStack {
+        public string Id { get { return item.Id; } }
+        int count = 0;
+        Item item;
+
+        public ItemStack(Item item, int count = 1) {
+            this.item = item;
+            this.count = count;
+        }
+
+        public void Add(Item item) {
+            count += 1;
+        }
+
+        public override string ToString() {
+            return string.Format("[ItemStack: Id={0}, count={1}]", Id, count);
+        }
+    }
+}
